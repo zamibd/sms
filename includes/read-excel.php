922d1718f9e56702279ffe9c404a9fd43c698f8f
zamibd <zamibd99@gmail.com>
2025-08-28 15:31:42 +0400

    some change

--- includes/read-excel.php
+++ includes/read-excel.php
@@ -1,23 +1,5 @@
 <?php
-/*
- * Copyright © 2018-2025 RBSoft (Ravi Patel). All rights reserved.
- *
- * Author: Ravi Patel
- * Website: https://rbsoft.org/downloads/sms-gateway
- *
- * This software is licensed, not sold. Buyers are granted a limited, non-transferable license
- * to use this software exclusively on a single domain, subdomain, or computer. Usage on
- * multiple domains, subdomains, or computers requires the purchase of additional licenses.
- *
- * Redistribution, resale, sublicensing, or sharing of the source code, in whole or in part,
- * is strictly prohibited. Modification (except for personal use by the licensee), reverse engineering,
- * or creating derivative works based on this software is strictly prohibited.
- *
- * Unauthorized use, reproduction, or distribution of this software may result in severe civil
- * and criminal penalties and will be prosecuted to the fullest extent of the law.
- *
- * For licensing inquiries or support, please visit https://support.rbsoft.org.
- */
+
 
 if (count(get_included_files()) == 1) {
     http_response_code(403);
